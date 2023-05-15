309074ece4c76c58d73b12dd8b99c0713179cd19
Aleksey Melnik <aleksey.melnik@zigzag.global>
2023-05-15 12:14:01 +0300

    Fix

--- js/app.js
+++ js/app.js
@@ -1,13 +1,9 @@
-let isAnimating         = false;
+let isAnimating = false;
 
 let xDown = null;
 let yDown = null;
 
-
-
-const bg                = document.querySelector('.fullpage');
-const portfolio         = document.querySelector('.portfolioWrapper');
-const blog              = document.querySelector('.blogWrapper')
+const bg = document.querySelector('.fullpage');
 
 const fadeOut = el => {
         el.style.opacity = 1;
@@ -41,13 +37,13 @@ let slideData = {
                 headColor: 'white',
                 className: '.mainSlide'
         },
-        // 1: {
-        //         bg_class: 'lightGray',
-        //         slideName: 'Портфолио',
-        //         headColor: 'black',
-        //         className: '.portfolio'
-        // },
         1: {
+                bg_class: 'lightGray',
+                slideName: 'Портфолио',
+                headColor: 'black',
+                className: '.portfolio'
+        },
+        2: {
                 bg_class: 'gray',
                 slideName: 'Услуги',
                 headColor: 'white',
@@ -59,19 +55,26 @@ let slideData = {
         //         headColor: 'black',
         //         className: '.aboutUs'
         // },
-        // 4: {
-        //         bg_class: "gray",
-        //         slideName: 'Блог',
-        //         headColor: 'white',
-        //         className: '.blog'
-        // },
-        2: {
+        3: {
+                bg_class: "gray",
+                slideName: 'Блог',
+                headColor: 'white',
+                className: '.blog'
+        },
+        4: {
                 bg_class: 'lightGray',
                 slideName: 'Контакты',
                 headColor: 'black',
                 className: '.contacts'
         }
 };
+
+const portfolio = () => {
+        document.querySelector('.portfolioWrapper').style.width = parseInt(document.querySelector('.slide_content').clientWidth / 372) * 372 + 'px';
+        
+}
+
+portfolio()
 
 const bgClass = (bgId, current) => {
         isAnimating = true;
@@ -83,17 +86,19 @@ const bgClass = (bgId, current) => {
         } = slideData[bgId];
         let curSlide = document.querySelector(slideData[current].className)
         let nextSldie = document.querySelector(className);
+
         if (className == '.mainSlide') {
-                fadeOut(document.querySelector('.progressBar'))
+                fadeOut(document.querySelector('.progressBar'));
         } else {
-                fadeIn(document.querySelector('.progressBar'))
+                fadeIn(document.querySelector('.progressBar'));
         }
-        curSlide.classList.remove('activeSlide')
-        nextSldie.classList.add('activeSlide')
+
+        curSlide.classList.remove('activeSlide');
+        nextSldie.classList.add('activeSlide');
         
         fadeOut(curSlide)
         setTimeout(() => {
-                fadeIn(nextSldie)
+                fadeIn(nextSldie);
         }, 1000);
 
         setTimeout(() => {
@@ -119,8 +124,8 @@ const handleTouchStart = evt => {
 };
 
 const handleTouchMove = evt => {
-        if (!xDown || !yDown) return;
-        if (isAnimating) return;
+        if (!xDown || !yDown) return
+        if (isAnimating) return
         let bgId = parseInt(bg.id);
         let xUp = evt.touches[0].clientX;
         let yUp = evt.touches[0].clientY;
@@ -129,7 +134,7 @@ const handleTouchMove = evt => {
 
         if (Math.abs(xDiff) < Math.abs(yDiff)) {
                 if (yDiff > 0) {
-                        if (bgId < 3) {
+                        if (bgId < Object.keys(slideData).length) {
                                 let nextBg = bgId + 1;
                                 bgClass(nextBg, bgId);
                                 bg.id = nextBg;
@@ -155,7 +160,7 @@ document.querySelector('.fullpage').addEventListener("wheel", event => {
         let bgId = parseInt(bg.id);
         if (isAnimating) return;
         if (delta == 1) {
-                if (bgId < 3) {
+                if (bgId < Object.keys(slideData).length) {
                         let nextBg = bgId + 1;
                         bgClass(nextBg, bgId);
                         bg.id = nextBg;
@@ -178,8 +183,8 @@ aboutUs.forEach(el => {
                 let next = document.querySelector("#" + el.classList[0]);
                 active.classList.remove('active_description');
                 next.classList.add('active_description');
-                document.querySelector('.' + el.classList[0]).classList.add('active_heading')
-                document.querySelector('.' + active.id).classList.remove('active_heading')
+                document.querySelector('.' + el.classList[0]).classList.add('active_heading');
+                document.querySelector('.' + active.id).classList.remove('active_heading');
                 fadeOut(active);
                 active.style.display = "none";
                 setTimeout(() => {
@@ -188,48 +193,18 @@ aboutUs.forEach(el => {
         })
 })
 
-const slideChanger = slider => {
-        let isDown = false;
-        let startX;
-        let scrollLeft;
-        slider.addEventListener('mousedown', (e) => {
-                isDown = true;
-                slider.classList.add('active');
-                startX = e.pageX - slider.offsetLeft;
-                scrollLeft = slider.scrollLeft;
-        });
-        slider.addEventListener('mouseleave', () => {
-                isDown = false;
-                slider.classList.remove('active');
-        });
-        slider.addEventListener('mouseup', () => {
-                isDown = false;
-                slider.classList.remove('active');
-        });
-        slider.addEventListener('mousemove', (e) => {
-                if (!isDown) return;
-                e.preventDefault();
-                const x = e.pageX - slider.offsetLeft;
-                const walk = (x - startX) * 3; //scroll-fast
-                slider.scrollLeft = scrollLeft - walk;
-        });
-}
-
-slideChanger(portfolio);
-slideChanger(blog)
-
 const listCount = document.querySelectorAll('.slides_counter li');
 
 listCount.forEach(el => {
         el.addEventListener('click', () => {
                 if (el.classList[1] != undefined || isAnimating) return;
-                let current = document.querySelector('.fullpage')
-                bgClass(parseInt(el.classList[0][6]), parseInt(current.id))
+                let current = document.querySelector('.fullpage');
+                bgClass(parseInt(el.classList[0][6]), parseInt(current.id));
                 current.id = parseInt(el.classList[0][6]);
         })
 });
 
-const input = document.querySelectorAll('.input2')
+const input = document.querySelectorAll('.input2');
 
 input.forEach(el => {
         el.addEventListener('blur', () => {
@@ -240,6 +215,21 @@ input.forEach(el => {
                 }
         })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // let portfolioWrapper = document.querySelector(".portfolioWrapper");
 // portfolioWrapper.mouseIsOver = false;
